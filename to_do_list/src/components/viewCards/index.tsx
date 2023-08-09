056cae103759d43dd5a96a97648e674757be33c4
Marcos Brito <njsudev5@hotmail.com>
2023-08-09 16:52:03 -0300

    alterando a cor de fundo do view cards

--- to_do_list/src/components/viewCards/index.tsx
+++ to_do_list/src/components/viewCards/index.tsx
@@ -6,8 +6,8 @@ interface PropsType {
 
 export default function ViewCards ({ title }: PropsType): React.JSX.Element {
   return (
-    <div className='flex w-[90%] lg:w-[30%] h-full border-2 border-Dark_3 rounded-xl'>
-      <h3>{title}</h3>
+    <div className='flex w-[90%] lg:w-[30%] h-full border-2 border-Dark_3 rounded-xl justify-center bg-Dark_2'>
+      <h3 className='text-lg text-Dark_5'>{title}</h3>
     </div>
   )
 }
